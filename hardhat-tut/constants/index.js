545f0a869e371e2b08beb25ff954b1338ffa040e
ak7270090 <ak7270090@gmail.com>
2022-09-11 14:37:59 +0530

    abi added

--- hardhat-tut/constants/index.js
+++ hardhat-tut/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0x7eAF252F9155bF3b9453510269B0362Fb0523dC9";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-l5xkcxu78-ak7270090.vercel.app/api";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
